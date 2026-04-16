775cbf3ba415586c08d143ab6c82d8b115c6619b
Shabbin <ambiance0100@gmail.com>
2026-04-17 01:14:15 +0600

    first commit

---
+++ lib/settings.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_SETTINGS = {
+  arabicFont: "amiri",
+  arabicFontSize: 36,
+  translationFontSize: 18,
+};
+
+export const ARABIC_FONTS = [
+  { label: "Amiri", value: "amiri" },
+  { label: "Scheherazade", value: "scheherazade" },
+];
